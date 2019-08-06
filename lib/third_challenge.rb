c732debabfbec5471b7e3364a5c3d497bd2c8b57
Matt Pitner <mattpitner09@gmail.com>
2019-08-06 20:01:35 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -19,14 +19,14 @@ def third_challenge
         },
       :hero_friends => [
       {
-        :name => "Benvolio",
-        :age => "17",
-        :attitude => "worried"
+        name: "Benvolio",
+        age: "17",
+        attitude: "worried"
       }, 
     {
-        :name => "Mercutio",
-        :age => "18",
-        :attitude => "hot-headed"
+        name: "Mercutio",
+        age: "18",
+        attitude: "hot-headed"
       }]
    }, 
    :capulet => {
@@ -42,20 +42,20 @@ def third_challenge
         },
       :heroine => {
         #your key/value pairs here
-        :name => "Juliet",
+        :name: "Juliet",
         :age => "15",
         :status => "alive"
         },
       :heroine_friends => [ 
         {
-          :name => "Steven",
-          :age => "30",
-          :attitude => "confused"
+          name: "Steven",
+          age: "30",
+          attitude: "confused"
         }, 
         {
-          :name => "Nurse",
-          :age => "44",
-          :attitude => "worried"
+          name: "Nurse",
+          age: "44",
+          attitude: "worried"
         }
         ]
    }
